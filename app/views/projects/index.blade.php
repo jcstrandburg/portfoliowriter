291a3168a1f9e57ce8b19fe13ca6af21553feab8
Justin Strandburg <justin.strandburg@hotmail.com>
2014-10-11 20:14:23 -0700

    Fixed resume url bug

--- app/views/projects/index.blade.php
+++ app/views/projects/index.blade.php
@@ -12,7 +12,7 @@
 				<div class='editable' data-operation='update-misc' data-field='{{$bio->name}}' data-id='{{$bio->id}}'>{{$bio->value}}</div>
 				{{link_to_route( 'misc.edit', "Edit Field", $bio->id, array('class'=>'enhanceHide'))}}
 				<h3>Resume URL</h3>				
-				<span class='editable' data-operation='update-misc' data-field='{{$resumeURL->name}}' data-id='{{$resumeURL->id}}'>{{$resumeURL->value}}</div>
+				<span class='editable' data-operation='update-misc' data-field='{{$resumeURL->name}}' data-id='{{$resumeURL->id}}'>{{$resumeURL->value}}</span>
 				{{link_to_route( 'misc.edit', "Edit Field", $resumeURL->id, array('class'=>'enhanceHide'))}}				
 			</div>
 		</div>
